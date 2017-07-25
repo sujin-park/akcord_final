6acf7b181c8be1f85c25bd66ef5ac1b264ca0c1f
BAEK <BAEK@DESKTOP-2QB86MI>
2017-07-25 20:07:10 +0900

    headline first edit

--- src/com/akcord/alarm/dao/AlarmDao.java
+++ src/com/akcord/alarm/dao/AlarmDao.java
@@ -7,6 +7,9 @@ public interface AlarmDao {
 	public void alarminsertreboard();		// alarm data of reboard article
 	public void alarminsertjoin();			// alarm data of group join
 	public void alarminsertwithdrawer();	// alarm data of group withdrawer
+	//좋아요
+	//공유 알림
+	
 	
 	// << alarm pop-up >>
 	public void alarmlist();	// shows list of alarms
