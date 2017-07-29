058c7ecfe9857416b8f62ad41f6c8c7da6920ec8
Owner <youje92@gmail.com>
2017-07-29 18:43:51 +0900

    dfsdfs

---
+++ src/com/akcord/user/model/MajorDto.java
@@ -0,0 +1,22 @@
+package com.akcord.user.model;
+
+public class MajorDto {
+	
+	private String major_id;
+	private String major_name;
+	
+	public String getMajor_id() {
+		return major_id;
+	}
+	public void setMajor_id(String major_id) {
+		this.major_id = major_id;
+	}
+	public String getMajor_name() {
+		return major_name;
+	}
+	public void setMajor_name(String major_name) {
+		this.major_name = major_name;
+	}
+
+	
+}
