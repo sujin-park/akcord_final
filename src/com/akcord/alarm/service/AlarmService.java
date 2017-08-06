a45bb5ca5375ee308f7f4699ea454879376a8595
BAEK <BAEK@DESKTOP-2QB86MI>
2017-08-06 15:34:42 +0900

    Alarm - Notice call function added

--- src/com/akcord/alarm/service/AlarmService.java
+++ src/com/akcord/alarm/service/AlarmService.java
@@ -1,5 +1,7 @@
 package com.akcord.alarm.service;
 
+import java.util.List;
+
 import com.akcord.alarm.model.AlarmDto;
 
 public interface AlarmService {
@@ -10,6 +12,9 @@ public interface AlarmService {
 	void alarminsertQnacomment(String target_id);		// 알람 DB 저장 - Qnacomment
 	void alarminsertQnalike(String target_id);			// 알람 DB 저장 - Qnalike
 	void alarminsertQnadislike(String target_id);		// 알람 DB 저장 - Qnadislike
+	
+	List<String> alarmMemberCall();	// 모든 멤버 호출
+	void alarminsertNotice(String target_id);// 공지사항 알림 전송
 	
 	AlarmDto alarmlist(String target_id);	// 알람 리스트 호출
 	int alarmNoneread(String target_id);	// 아직 안 읽은 알람
